7323a1b54ad18f2cc197501940d606044a6812f3
davodhiambo <jesseomolo@yahoo.com>
2024-07-06 12:06:31 +0300

    New Feature - Add aboutHandler() function to main package - execute aboutHandler() in main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my lemonade stand")	
+	fmt.Fprintf(w, "Welcome to my lemonade stand")
 }
 
 func menuHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,11 +16,17 @@ func menuHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Freshly-baked cup cakes: $5\n")
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About Us\n")
+	fmt.Fprintf(w, "The Lemonade Inc was started in 2008 by David Odhiambo out of his sheer passion for lemonade. Over the years, the Lemonade Inc has contributed massively to the bevarage industry. It revolutionized how we approach and consume lemonade, introducing the world to the twirling straw.\n")
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("static"))
 	handler := http.StripPrefix("/static/", fileServer)
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/menu", menuHandler)
+	http.HandleFunc("/about", aboutHandler)
 	http.Handle("/static/", handler)
 	http.ListenAndServe(":8080", nil)
 }
