44068d4fab6069671b5ae86aad2cd914c797bfce
davodhiambo <jesseomolo@yahoo.com>
2024-07-06 11:40:54 +0300

    fix: add '\n' at the end of every line of menuHandle()

--- main.go
+++ main.go
@@ -10,16 +10,17 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func menuHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Below is our menu:")
-	fmt.Fprintf(w, "Jug of lemonade: $50")
-	fmt.Fprintf(w, "Glass of lemonade: $10")
-	fmt.Fprintf(w, "Freshly-baked cup cakes: $5")
+	fmt.Fprintf(w, "Below is our menu:\n")
+	fmt.Fprintf(w, "Jug of lemonade: $50\n")
+	fmt.Fprintf(w, "Glass of lemonade: $10\n")
+	fmt.Fprintf(w, "Freshly-baked cup cakes: $5\n")
 }
 
 func main() {
 	fileServer := http.FileServer(http.Dir("static"))
 	handler := http.StripPrefix("/static/", fileServer)
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/menu", menuHandler)
 	http.Handle("/static/", handler)
 	http.ListenAndServe(":8080", nil)
 }
