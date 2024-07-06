b133571fc83684ebed4f82e8d969969850c0edf3
davodhiambo <jesseomolo@yahoo.com>
2024-07-06 11:24:06 +0300

    new feature - added menuhandler()

--- main.go
+++ main.go
@@ -6,14 +6,20 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my lemonade stand")
-	
+	fmt.Fprintf(w, "Welcome to my lemonade stand")	
+}
+
+func menuHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Below is our menu:")
+	fmt.Fprintf(w, "Jug of lemonade: $50")
+	fmt.Fprintf(w, "Glass of lemonade: $10")
+	fmt.Fprintf(w, "Freshly-baked cup cakes: $5")
 }
 
 func main() {
 	fileServer := http.FileServer(http.Dir("static"))
 	handler := http.StripPrefix("/static/", fileServer)
-	http.HandleFunc("/", greet)
+	http.HandleFunc("/", homeHandler)
 	http.Handle("/static/", handler)
 	http.ListenAndServe(":8080", nil)
 }
