8d0e6313655d4397f71dc1ce7a794c4bf5c6d415
davodhiambo <jesseomolo@yahoo.com>
2024-07-06 10:44:12 +0300

    Refactor greet function - change name of greet() function to homeHandle() - change display massage to "Welcome to my lemonade stand"

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	//"time"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my first web server")
-	//fmt.Fprintf(w, "Hello World! %s", time.Now())
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my lemonade stand")
+	
 }
 
 func main() {
