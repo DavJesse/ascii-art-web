ba9717afcd941ac84908e946cad0f3c69082ab14
davodhiambo <jesseomolo@yahoo.com>
2024-07-10 11:19:17 +0300

    feature: add SubmitFormHandler() to webPages.go in Web package

--- Web/webPages.go
+++ Web/webPages.go
@@ -1,7 +1,20 @@
 package Web
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func FormHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/submitForm.html")
+}
+
+func SubmitFormHandler(w http.ResponseWriter, r http.Request) (string, string) {
+	var bnStyle, inputStr string
+	if r.Method == http.MethodPost {
+		bnStyle = r.FormValue("style")
+		inputStr = r.FormValue("inputStr")
+	} else {
+		http.Error(w, "Invalid Request Method!", http.StatusMethodNotAllowed)
+	}
+	return bnStyle, inputStr
 }
