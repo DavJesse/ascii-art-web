bddb0afc5883f1ddcb456a52f05e20b00ebf0c3b
davodhiambo <jesseomolo@yahoo.com>
2024-07-10 18:15:14 +0300

    feature: implemet select feature in submitForm.html

--- Web/webPages.go
+++ Web/webPages.go
@@ -2,19 +2,28 @@ package Web
 
 import (
 	"net/http"
+	"strings"
+	"text/template"
+	"web/Lib"
 )
 
 func FormHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/submitForm.html")
 }
 
-func SubmitFormHandler(w http.ResponseWriter, r http.Request) (string, string) {
+func SubmitFormHandler(w http.ResponseWriter, r *http.Request) {
 	var bnStyle, inputStr string
 	if r.Method == http.MethodPost {
+
 		bnStyle = r.FormValue("style")
 		inputStr = r.FormValue("inputStr")
+
+		output := Lib.AsciiArt("", "", "", inputStr, inputStr, bnStyle+".txt")
+		output = strings.ReplaceAll(output, "\n", "<br>")
+
+		tmpl := template.Must(template.ParseFiles("static/printer.html"))
+		tmpl.Execute(w, struct{ AsciiArt string }{AsciiArt: output})
 	} else {
 		http.Error(w, "Invalid Request Method!", http.StatusMethodNotAllowed)
 	}
-	return bnStyle, inputStr
 }
